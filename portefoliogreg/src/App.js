7fa32305184f8b00b747fdd78d7b893bc138d3f8
Lowco972 <gregory.vaste@gmail.com>
2021-09-20 11:40:44 +0200

    commit

--- portefoliogreg/src/App.js
+++ portefoliogreg/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import './App.css';
 
-function App() {
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import knowledges from './pages/Knowledges';
+import NotFound from './pages/NotFound';
+import Portefolio from './pages/Portefolio';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        {/* navigation */}
+        <switch>
+        
+          <Route path="/" exact component={Home}/>
+          <Route path="/compétences" exact component={knowledges}/>
+          <Route path="/portfolio" exact component={Portefolio}/>
+          <Route path="contact" exact component={Contact}/>
+          <Route component={NotFound}/>
+        </switch>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
---
+++ portefoliogreg/src/pages/Knowledges.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const knowledges = () => {
+    return (
+        <div>
+            knowledges
+        </div>
+    );
+};
+
+export default knowledges;
